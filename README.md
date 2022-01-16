015f7be1db440a64c1c57d2cec38be9071a8c47f
Doomnoodles <doomnoodles@hotmail.com>
2022-01-16 16:55:54 +0200

    Major rewrite

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
 # estonian-spam-list
 Spammy estonian sites adblock list
 
+This repository contains a script to automatically detect Aliexpress redirects and add them to ```hosts.txt```.
+Its also possible to manually add entries to  ```hosts.txt```. 
+NB: ```sites.txt``` is generated from ```hosts.txt``` and may not be edited.
+
 [Click here to subscribe to the list](https://subscribe.adblockplus.org/?location=https://raw.githubusercontent.com/doomnoodles/estonian-spam-list/main/sites.txt&title=Estonian%20spam-sites%20list)
 
-## Google bookmarklet
-Easily copy every link on a Google search results page that doesnt contain "google"
-
-1. Copy the code from [bookmarklet_google.js](https://github.com/doomnoodles/estonian-spam-list/blob/main/bookmarklet_google.js)
-2. Create a new bookmark in your browser. In the URL field, type  ```javascript:``` and then right after that paste the code
-
-## Workflow
+## Aliexpress detection workflow
 
 1. Execute ```npm install```
 2. Create a file named ```unverified_links.txt```
 3. Gather links from google using the bookmarklet and paste them in the file or write one url per line manually
 4. Execute ```npm run verify```
-5. Results are in the ```results``` folder
-6. Use some list deduplicating tool to merge with ```sites.txt```
+5. Run results are in the ```results``` folder, and have been merged with ```hosts.txt``` and ```sites.txt```
+
+## Bookmarklet
+Easily copy every link on a Google search results page that doesnt contain "google"
+
+1. Copy the code from [bookmarklet_google.js](https://github.com/doomnoodles/estonian-spam-list/blob/main/bookmarklet_google.js)
+2. Create a new bookmark in your browser. In the URL field, type  ```javascript:``` and then right after that paste the code
 
 ## TODO
 - [x] Bookmarklet to copy all links from a google search page
 - [x] Script to check where the links go
 - [ ] Improve workflow/reliability
+- [x] Sites that look like et.xxxx.info should be made into a list that can be manually reviewed
+- [x] Whitelist site if host contains aliexpress
--- verify.js
+++ verify.js
@@ -1,76 +1,162 @@
 import { readFileSync, writeFileSync, existsSync, mkdirSync } from "fs";
+import { PromisePool } from '@supercharge/promise-pool'
 import puppeteer from 'puppeteer';
 
-const FORBIDDEN_HOSTS = ["aliexpress.com"];
+const ADBLOCK_FILTER_HEADER = `[Adblock Plus 2.0]
+! Title: Estonian spam sites list
+! Version: 20220115
+! Expires: 1 days
+! Homepage: https://github.com/doomnoodles/estonian-spam-list
+! License: CC0 1.0 Universal http://creativecommons.org/publicdomain/zero/1.0/legalcode
+
+`;
+
+const DONT_VERIFY_IF_HOST_CONTAINS = ["aliexpress", "google", "youtube", "wikipedia"];
+const FORBID_REDIRECTS_TO = ["aliexpress.com"];
 const RESULTS_DIR = "./results/";
 
-const VISIT_TIMEOUT = 10000; // 10 seconds
-const SIMULTANEOUS_VISITS = 10;
-const urls = readFileSync("unverified_links.txt", { encoding: 'utf8' })?.trim()?.split(/\r?\n/);
+const TAB_TIMEOUT = 10000; // 10 seconds
+const MAX_BROWSER_TABS = 5;
 
-const browser = await puppeteer.launch({ headless: true });
+class LoggerArray {
+    #interval;
+    #maxSize;
+    #arr;
 
-let resolvedUrls = [];
-let visitedCount = 0;
-const totalCount = urls.length;
-console.log(`Visiting ${totalCount} urls...`);
-while(true) {
-    const chunkedUrls = urls.splice(0, SIMULTANEOUS_VISITS);
-    visitedCount += chunkedUrls.length;
-    if(chunkedUrls.length === 0) break;
-
-    resolvedUrls = [...resolvedUrls, ...await Promise.all(chunkedUrls.map(startUrl => new Promise(async res => {
-        try {
-            const page = await browser.newPage();
-            await page.goto(startUrl, { referer: "https://www.google.com/", timeout: VISIT_TIMEOUT, waitUntil: "networkidle2" });
-            const endUrl = await page.url();
-            await page.close();
-
-            res({ startUrl, endUrl });
-        } catch(error) {
-            res({ startUrl, error });
+    constructor(interval, maxSize) {
+        this.#interval = interval;
+        this.#maxSize = maxSize;
+        this.#arr = [];
+    }
+    push(element) {
+        this.#arr.push(element);
+        if(!(this.#arr.length % this.#interval)) {
+            console.info(`Visited ${this.#arr.length}/${this.#maxSize} urls`);
         }
-    })))];
-
-    console.log(`Visited ${visitedCount}/${totalCount} urls`);
-}
-
-let spamUrls = [];
-let otherUrls = [];
-
-for(let data of resolvedUrls) {
-    top: {
-        const { startUrl, endUrl, error } = data;
-        if(error) {
-            console.error(`Encountered error at '${startUrl}': \n${error.toString()}`)
-            otherUrls.push(data);
-            break top;
-        }
-
-        const { host } = new URL(endUrl);
-        for(let forbiddenHost of FORBIDDEN_HOSTS) {
-            if(host.includes(forbiddenHost)) {
-                spamUrls.push(data);
-                break top;
-            }
-        }
-        otherUrls.push(data);
+    }
+    asArray() {
+        return this.#arr;
     }
 }
 
+class PuppeteerPool {
+    #maxTabs;
+    #timeout;
+    #browser;
+    #visited;
+
+    constructor(maxTabs, timeout) {
+        this.#maxTabs = maxTabs;
+        this.#timeout = timeout;
+    }
+    async visit(urls) {
+        console.info(`Visiting ${urls.length} urls...`)
+        this.#browser = await puppeteer.launch({ headless: true });
+        this.#visited = new LoggerArray(10, urls.length);
+        const res = await PromisePool.for(urls)
+        .withConcurrency(this.#maxTabs)
+        .process(async startUrl => {
+            try {
+                const page = await this.#browser.newPage();
+                await page.goto(startUrl, { referer: "https://www.google.com/", timeout: this.#timeout, waitUntil: "networkidle2" });
+                const endUrl = await page.url();
+                await page.close();
+                this.#visited.push(startUrl);
+    
+                return { startUrl, endUrl };
+            } catch(error) {
+                console.error(`Error at ${startUrl}: \n\t${error.message}`);
+                this.#visited.push(startUrl);
+                return { startUrl, error };
+            }
+        });
+        this.#browser.close();
+        return res;
+    }
+}
+
+function isForbiddenUrl(resolvedUrl) {
+    const { endUrl, error } = resolvedUrl;
+    if(error) return false;
+
+    const { host } = new URL(endUrl);
+    for(let forbiddenHost of FORBID_REDIRECTS_TO) {
+        if(host.includes(forbiddenHost)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function splitResolvedUrls(resolvedUrls) {
+    let forbiddenUrls = [];
+    let otherUrls = [];
+
+    for(let resolvedUrl of resolvedUrls) {
+        if(isForbiddenUrl(resolvedUrl)) {
+            forbiddenUrls.push(resolvedUrl);
+        } else {
+            otherUrls.push(resolvedUrl);
+        }
+    }
+    return { forbiddenUrls, otherUrls };
+}
+
 const wwwRegex = /^(www\.)/;
-const timestamp = Date.now();
-const spamHosts = Array.from(new Set(spamUrls.map(data => new URL(data.startUrl).host.replace(wwwRegex, ""))));
+const etRegex = /^(et\.)/;
+function removeRelevantSubdomains(str) {
+    return str.replace(wwwRegex, "").replace(etRegex, "");
+}
+
+function getHosts(filename) {
+    try {
+        return readFileSync(filename, { encoding: 'utf8' })?.trim()?.split(/\r?\n/);
+    } catch(e) {
+        return [];
+    }
+}
+
+const previousHosts = new Set(getHosts("hosts.txt"));
+const lines = readFileSync("links_unverified.txt", { encoding: 'utf8' })?.trim()?.split(/\r?\n/);
+const urls = Array.from(new Set(lines.filter(url => {
+    const host = new URL(url).host;
+    for(let str of DONT_VERIFY_IF_HOST_CONTAINS)
+        if(host.includes(str)) 
+            return false;
+    if(previousHosts.has(host))
+        return false;
+    return true;
+})));
+
+console.info(`Detected ${lines.length} lines of input...`)
+if(lines.length !== urls.length) {
+    console.info(`Filtered out ${lines.length - urls.length} lines...`)
+}
+
+const resolvedUrls = (await new PuppeteerPool(MAX_BROWSER_TABS, TAB_TIMEOUT).visit(urls)).results;
+const { forbiddenUrls, otherUrls } = splitResolvedUrls(resolvedUrls);
+
+const suspiciousUrls = otherUrls.filter(resolvedUrl => {
+    return resolvedUrl.startUrl.startsWith("et.");
+}).map(resolvedUrl => resolvedUrl.startUrl);
 
 if (!existsSync(RESULTS_DIR)){
     mkdirSync(RESULTS_DIR, { recursive: true });
 }
 
-writeFileSync(`${RESULTS_DIR}${timestamp}-other.json`, JSON.stringify(otherUrls, null, 2));
-writeFileSync(`${RESULTS_DIR}${timestamp}-urls.json`, JSON.stringify(spamUrls, null, 2));
-writeFileSync(`${RESULTS_DIR}${timestamp}-hosts.txt`, spamHosts.join("\n"));
+const timestamp = Date.now();
+const newForbiddenHosts = Array.from(new Set(forbiddenUrls.map(data => new URL(data.startUrl).host)));
 
-// console.log(spamUrls)
-console.log(`Spam url rate: ${spamUrls.length}/${totalCount}`);
-console.log("Done.")
-await browser.close();
+const forbiddenHosts = Array.from(new Set([...previousHosts, ...newForbiddenHosts])).sort();
+
+writeFileSync(`hosts.txt`, forbiddenHosts.join("\n"));
+writeFileSync(`sites.txt`, ADBLOCK_FILTER_HEADER + forbiddenHosts.map(host => removeRelevantSubdomains(host)).sort().join("\n"));
+writeFileSync(`${RESULTS_DIR}${timestamp}.json`, JSON.stringify(resolvedUrls, null, 2));
+writeFileSync(`${RESULTS_DIR}${timestamp}-hosts.txt`, newForbiddenHosts.join("\n"));
+if(suspiciousUrls.length) {
+    console.info(`Suspicious url rate: ${suspiciousUrls.length}/${resolvedUrls.length}`);
+    writeFileSync(`${RESULTS_DIR}${timestamp}-suspicious.txt`, suspiciousUrls.join("\n"));
+}
+
+console.info(`Spam url rate: ${forbiddenUrls.length}/${resolvedUrls.length}`);
+console.info("Done.")
